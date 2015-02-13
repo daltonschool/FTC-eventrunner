66b34b5d3e4f7a8e84c1857f58127564457f695f
Davis Haupt <dbh937@gmail.com>
2015-02-13 14:14:15 -0500

    inputting a csv into the textarea spits out the JSON in the console. not much.

--- FTC-eventrunner.js
+++ FTC-eventrunner.js
@@ -1,23 +1,41 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter',0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            if (Meteor.userId())
+                return Meteor.users.findOne({_id: Meteor.userId()}).profile.counter;
+            else
+                return Session.get('counter');
+        }
+    });
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            if (Meteor.userId()) {
+                Meteor.users.update({_id: Meteor.userId()}, {$inc: {'profile.counter': 1}});
+            }
+            else {
+                Session.set('counter', Session.get('counter') + 1);
+            }
+        },
+        'submit .new-task': function(event) {
+            var text = event.target.text.value;
+            console.log(Papa.parse(text));
+            return false;
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+    });
 }
