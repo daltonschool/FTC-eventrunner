9ef3cdaa5b8ade7bbc76798a276fae3eb8aa5028
davis <dbh937@gmail.com>
2015-03-05 10:12:25 -0500

    more stuff

--- client/body.js
+++ client/body.js
@@ -1,3 +1,16 @@
 /**
  * Created by davis on 3/4/15.
  */
+Template.body.helpers({
+  "getEvent": function() {
+    var s = Session.get("eventID");
+    if (s)
+      return s;
+    else
+      return null;
+  }
+});
+
+Template.body.events({
+
+});
